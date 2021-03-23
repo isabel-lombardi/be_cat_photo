ba82b2ee3a58c16286c82bce15e1c9563343715c
isabel <isabel.lombardi96@gmail.com>
2021-03-23 08:49:45 +0100

    [FEAT] signup method close #2

--- cat_photo/urls.py
+++ cat_photo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from account.views import SignupAPIView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path("signup/", SignupAPIView.as_view()),
+
 ]

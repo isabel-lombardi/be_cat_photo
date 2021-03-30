8aae93fa37d68dca45eb3562850feddb71d4b3ad
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-03-31 00:00:52 +0200

    [UPLOAD] resize image close #18

--- upload/views.py
+++ upload/views.py
@@ -5,9 +5,32 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
 from rest_framework import status
 from .serializers import ImageSerializer
-
+from PIL import Image
+import os
+from django.conf import settings
 
 from upload.classification.classification import Classification
+
+
+def image_resize(file_path_rel):
+    file_path_rel = os.path.normpath(file_path_rel)
+    # removes any initial slash
+    if file_path_rel[0] != os.sep:
+        file_path_rel = file_path_rel[1:]
+    file_path_abs = os.path.join(os.path.normpath(settings.BASE_DIR), file_path_rel)
+    # print("file_path_abs ", file_path_abs)
+
+    file_name, file_extension = os.path.splitext(file_path_abs)
+    file_path_thumb_abs = os.path.join(file_name + '_thumbnail' + file_extension)
+    # print("file_path_thumb_abs:", file_path_thumb_abs)
+    try:
+        image = Image.open(file_path_abs)
+        image.thumbnail((400, 400))
+        image.save(file_path_thumb_abs)
+        return True
+    except OSError:
+        print('cannot open', file_path_abs)
+        return False
 
 
 class UploadAPIView(APIView):
@@ -44,6 +67,9 @@ class UploadAPIView(APIView):
             obj.result = classification.use_template()
 
             serializer.save()
+
+            if not image_resize(serializer.data['image']):
+                return Response(serializer.errors, status=status.HTTP_507_INSUFFICIENT_STORAGE)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

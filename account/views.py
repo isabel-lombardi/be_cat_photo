552e0f1a2611caf63b915f72311d00584e41994c
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-04-01 16:13:39 +0200

    [LOGOUT] logout method close #47

--- account/views.py
+++ account/views.py
@@ -7,14 +7,20 @@ from rest_framework.views import APIView  # class view
 
 class SignupAPIView(APIView):
 
-
     def post(self, request):
         serializer = UserSerializer(data=request.data)  # get data from request
 
         if serializer.is_valid():  # check if serializer is valid
 
-
             serializer.save()
 
-            return Response('Correctly registrated', status=status.HTTP_201_CREATED)
+            return Response('Correctly registered', status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAPIView(APIView):
+
+    def get(self, request):
+        # simply delete the token to force a login
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)

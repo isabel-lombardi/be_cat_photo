511a23e29f6b01c1fec881a11804b2842dd11595
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-04-02 19:37:57 +0200

    [UPLOAD] image validation close #50 bis

--- upload/serializers.py
+++ upload/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import Image
+from PIL import Image as PilImage
 
 
 class ImageSerializer(ModelSerializer):
@@ -8,9 +9,15 @@ class ImageSerializer(ModelSerializer):
         model = Image       # model to serialize
         fields = ["result", "user", "image"]  # How many fields to display ("__all__")
 
-
-
-
-
-
-
+    def validate_image(self, image_file):
+        """
+        Check that the image format is Jpeg or Png.
+        """
+        try:
+            img = PilImage.open(image_file)
+        except OSError:
+            raise ValidationError("Cannot open", image_file)
+        print("Image format", img.format)
+        if not img.format in ['JPEG', 'PNG']:
+            raise ValidationError("Image format is neither Jpeg nor Png")
+        return image_file
